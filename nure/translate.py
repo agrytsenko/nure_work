ff186af1142b8d56b21fbd2504cf75bfe6de62eb
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-02-17 20:43:52 +0200

    finished with json translator

--- nure/translate.py
+++ nure/translate.py
@@ -9,7 +9,6 @@ from HTMLParser import HTMLParser
 from requests.adapters import HTTPAdapter
 
 
-
 # from bs4 import BeautifulSoup
 #
 # http://py-translate.readthedocs.io/en/latest/devs/api.html
@@ -19,12 +18,9 @@ from requests.adapters import HTTPAdapter
 #
 # http://stackoverflow.com/questions/14369447/how-to-save-back-changes-made-to-a-html-file-using-beautifulsoup-in-python
 #
-# u'[[["你好！","hello there!",,,1]],,"en",,,,1,,[["en"],,[1],["en"]]]'
-#
 
 URL = 'http://translate.google.com/m?hl=%s&sl=%s&q=%s'
 URL_JSON = 'https://translate.google.com/translate_a/single'
-RES_PATT = re.compile(r'^\[\[\["(.+)","')
 
 user_agent = (
     "Mozilla/4.0 ("
@@ -85,15 +81,18 @@ def json_translate(text, to_lang='auto', from_lang='auto'):
         headers={'User-Agent': user_agent},
         params=params)
     prepare = session.prepare_request(request)
-    response = session.send(prepare, verify=True)
+    resp = session.send(prepare, verify=True)
 
-    if response.status_code != requests.codes.ok:
-        response.raise_for_status()
+    if resp.status_code != requests.codes.ok:
+        resp.raise_for_status()
 
-    match = re.match(RES_PATT, response.content.decode('utf-8'))
-    if match:
-        return match.groups()[0]
-    return ''
+    json_fix = re.sub(r',+', ',', resp.content.decode('utf-8'))
+    try:
+        res = json.loads(json_fix)[0]
+    except (ValueError, IndexError):
+        return ''
+    translations = map(lambda l: l[0], res)
+    return ''.join(translations)
 
 
 if __name__ == '__main__':
@@ -101,7 +100,7 @@ if __name__ == '__main__':
     tt = """
 1. Links to FAQs in-app will use the encrypted app_id as we do now
 2. App will register for encrypted app_id as it does now
-3. In FB and WebC sends the URL will be `https://dl.agent.ai/{encrypted_appgroup_id}/faq/{faq_id}`
+3. In FB and WebC sends the URL will be `https:/my.website.com/{encrypted_appgroup_id}/faq/{faq_id}`
 3a. The dl.agent.ai handler will redirect the {encrypted_appgroup_id} to HelpCenter based on {faq_id}
 4. We will backlog a task for SDKs to support handlers for {encrypted_appgroup_id}
 5. Over time we will deprecate app_id but there is a lot of backend code that deals with this and it would be a LOT of work to just remove it
@@ -112,12 +111,14 @@ b. Handler for deeplinks on the agent.ai server needs to support both {encrypted
 c. When client is Facebook or WebClient we should send links using the {encrypted_appgroup_id}
 d. The Landing Page code will use the {encrypted_app_id}"""
 
-    for _ in range(10):
-        print json_translate('hello there!', 'zh_cn')
-
     translate = json_translate
+
+    for _ in range(10):
+        print translate('hello there!', 'zh_cn')
+
     print translate('hello there!', 'ja')
     print translate('hello there!', 'ru')
 
-    for t in tt.split('\n'):
-        print translate(t, 'ru')
+    # for t in tt.split('\n'):
+    print translate(tt, 'ru')
+    # for _ in range(5): print translate(tt, 'ru')
